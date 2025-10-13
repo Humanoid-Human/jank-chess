65b867fc83a5edda33912fbb7f2d85dc18ce4c73
humanoid-human <trevorxu5@gmail.com>
2025-10-12 22:17:57 -0400

    progress on legal moves get

--- src/board.rs
+++ src/board.rs
@@ -1,22 +1,18 @@
 use crate::util::{*, Colour::*, PieceType::*};
 
-#[derive(Clone, Copy)]
-pub struct Piece {
-    pub colour: Colour,
-    pub piece_type: PieceType,
-    pub can_en_passant: bool
+pub struct CastleInfo {
+    kingside: bool,
+    queenside: bool
 }
 
-impl Piece {
-    pub fn new(colour: Colour, piece_type: PieceType) -> Piece {
-        Piece { colour, piece_type, can_en_passant: false }
-    }
+impl CastleInfo {
+    fn new() -> CastleInfo { CastleInfo { kingside: true, queenside: true } }
 }
 
 struct Board {
     pieces: [Option<Piece>; 64],
-    white_castle: bool,
-    black_castle: bool,
+    white_castle: CastleInfo,
+    black_castle: CastleInfo,
     turn: Colour
 }
 
@@ -43,33 +39,33 @@ impl Board {
             pieces[rankfile_to_board((7, i))] = Some(Piece::new(Black, a[i as usize]));
         }
 
-        Board { pieces, white_castle: true, black_castle: true, turn: White }
+        Board { pieces, white_castle: CastleInfo::new(), black_castle: CastleInfo::new(), turn: White }
     }
 
     pub fn get_legal_moves(&self, colour: Colour) -> Vec<Move> {
         let mut out: Vec<Move> = Vec::new();
 
-        for maybe_piece in &self.pieces {
-            if let Some(piece) = maybe_piece {
-                match piece.piece_type {
+        for (pos, maybe_piece) in self.pieces.iter().enumerate() {
+            if let Some(piece) = maybe_piece && piece.colour == colour {
+                match piece.piece {
                     Rook => {
-                        self.get_rook_moves(piece, &mut out);
+                        self.get_rook_moves(pos, piece.colour);
                     },
                     Bishop => {
-                        self.get_bishop_moves(piece, &mut out);
+                        self.get_bishop_moves(pos, piece.colour);
                     },
                     Queen =>  {
-                        self.get_rook_moves(piece, &mut out);
-                        self.get_bishop_moves(piece, &mut out);
+                        self.get_rook_moves(pos, piece.colour);
+                        self.get_bishop_moves(pos, piece.colour);
                     },
                     Knight =>  {
-                        self.get_knight_moves(piece, &mut out);
+                        self.get_knight_moves(pos, piece.colour);
                     },
                     King => {
-                        self.get_king_moves(piece, &mut out);
+                        self.get_king_moves(pos, piece.colour);
                     },
                     Pawn => {
-                        self.get_pawn_moves(piece, &mut out);
+                        self.get_pawn_moves(pos, piece.colour);
                     }
                 }
             }
@@ -78,27 +74,73 @@ impl Board {
         out
     }
 
-    pub fn get_piece_at(&self, pos: (u8, u8)) -> Option<Piece> {
-        self.pieces[rankfile_to_board(pos) as usize]
+    pub fn piece_at(&self, pos: (u8, u8)) -> Option<Piece> {
+        self.pieces[rankfile_to_board(pos)]
     }
 
-    fn get_rook_moves(&self, piece: &Piece, out: &mut Vec<Move>) {
+    // TODO
+    pub fn is_check_after(&self, moove: Move, colour: Colour) -> bool { false }
 
+    fn get_rook_moves(&self, pos: usize, colour: Colour) -> Vec<Move> {
+        let mut moves: Vec<(u8, u8)> = Vec::new();
+        let rfpos = board_to_rankfile(pos);
+
+        let bounds = [|x: u8| x > 0, |x: u8| x < 7];
+        let it = [|x: &mut u8| *x -= 1, |x: &mut u8| *x += 1];
+
+        for i in 0..2 {
+            let mut phori = rfpos.1;
+            while bounds[i](phori) {
+                it[i](&mut phori);
+                let maybe_piece = self.piece_at((rfpos.0, phori));
+                if let Some(piece) = maybe_piece {
+                    if piece.colour == colour {
+                        break;
+                    } else {
+                        moves.push((rfpos.0, phori));
+                        break;
+                    }
+                } else {
+                    moves.push((rfpos.0, phori));
+                }
+            }
+
+            let mut pvert = rfpos.0;
+            while bounds[i](pvert) {
+                it[i](&mut pvert);
+                let maybe_piece = self.piece_at((pvert, rfpos.1));
+                if let Some(piece) = maybe_piece {
+                    if piece.colour == colour {
+                        break;
+                    } else {
+                        moves.push((pvert, rfpos.1));
+                        break;
+                    }
+                } else {
+                    moves.push((pvert, rfpos.1));
+                }
+            }
+        }
+
+        moves.retain(
+            |x| !self.is_check_after(Move{start: rfpos, end: *x}, colour));
+        moves.iter().map(|x| Move {start: rfpos, end: *x}).collect()
     }
 
-    fn get_bishop_moves(&self, piece: &Piece, out: &mut Vec<Move>) {
-        
+    // TODO
+    fn get_bishop_moves(&self, pos: usize, colour: Colour) -> Vec<Move> {
+        Vec::new()
     }
 
-    fn get_king_moves(&self, piece: &Piece, out: &mut Vec<Move>) {
-
+    fn get_king_moves(&self, pos: usize, colour: Colour) -> Vec<Move> {
+        Vec::new()
     }
 
-    fn get_knight_moves(&self, piece: &Piece, out: &mut Vec<Move>) {
-
+    fn get_knight_moves(&self, pos: usize, colour: Colour) -> Vec<Move> {
+        Vec::new()
     }
 
-    fn get_pawn_moves(&self, piece: &Piece, out: &mut Vec<Move>) {
-
+    fn get_pawn_moves(&self, pos: usize, colour: Colour) -> Vec<Move> {
+        Vec::new()
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,18 +1,29 @@
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub enum Colour { White, Black }
-#[derive(Clone, Copy)]
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub enum PieceType { King, Queen, Rook, Bishop, Knight, Pawn }
 
 #[derive(Clone, Copy)]
 pub struct Move {
-    start: (u8, u8),
-    end: (u8, u8)
+    pub start: (u8, u8),
+    pub end: (u8, u8)
 }
 
-pub fn board_to_rankfile(p: u8) -> (u8, u8) {
-    return (p / 8, p % 8);
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct Piece {
+    pub piece: PieceType,
+    pub colour: Colour
+}
+
+impl Piece {
+    pub fn new(colour: Colour, piece: PieceType) -> Piece { Piece { piece, colour } }
+}
+
+pub fn board_to_rankfile(p: usize) -> (u8, u8) {
+    ((p / 8) as u8, (p % 8) as u8)
 }
 
 pub fn rankfile_to_board(p: (u8, u8)) -> usize {
-    return (p.0 * 8 + p.1) as usize;
+    (p.0 * 8 + p.1) as usize
 }
